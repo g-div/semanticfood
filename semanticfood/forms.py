0598f6cf55be6907f4341b03d5d314842ba0a9ea
Tim <Tim Eulitz>
2015-09-29 00:41:42 +0200

    Recipe Create Form - WIP

--- semanticfood/forms.py
+++ semanticfood/forms.py
@@ -7,6 +7,5 @@ class RecipeForms(Form):
     prepTime = DateTimeField('Preparation Time', format='%H:%M')
     cookTime = DateTimeField('Cooking Time', format='%H:%M')
     serving = IntegerField('Serving', [validators.NumberRange(min=0)])
-    ingredient = FieldList(TextAreaField('Ingredients'), min_entries=1)
     instructions = TextAreaField('Instructions')
     submit = SubmitField('Save')
