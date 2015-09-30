6ab2c08cee52a91caa3e7029bb02917b64eedaff
g-div <g-div@anche.no>
2015-10-01 01:58:04 +0200

    fix schema.org uri

--- semanticfood/config.py
+++ semanticfood/config.py
@@ -8,5 +8,9 @@ GRAPH_NAME = 'http://semanticfood.org/recipes/'
 SPARQL_ENDPOINT = 'http://46.101.152.77:8890/sparql'
 
 ONTO = {
-	'BBC' : 'http://www.bbc.co.uk/ontologies/fo/'
+    'BBC': 'http://www.bbc.co.uk/ontologies/fo/',
+    'SCHEMA': 'https://schema.org/'
 }
+
+USDA_API = 'http://api.nal.usda.gov/ndb/reports/?ndbno={1}&api_key={0}'
+USDA_API_KEY = 'V5zvO2rQuQAq36ajpuFhTaPLm74RsN9CrcCP3YG1'
