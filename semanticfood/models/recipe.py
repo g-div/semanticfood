a9a37c4bbd56d842314b4f1d99e99c84a87e2364
g-div <g-div@anche.no>
2015-10-04 19:30:57 +0200

    add nutrition informations

--- semanticfood/models/recipe.py
+++ semanticfood/models/recipe.py
@@ -29,7 +29,7 @@ class Recipe():
         self.ingredients = []
 
         for response in USDA(data.get('ingredient')).getData():
-            self.ingredients.append(Ingredient(name=response.get('report').get('food').get('name'), 
+            self.ingredients.append(Ingredient(name=response.get('report').get('food').get('name'),
                                                nutrients=response.get('report').get('food').get('nutrients')))
 
         self.uri = LOCAL[quote(self.name)]
@@ -37,20 +37,42 @@ class Recipe():
     def serialize(self):
 
         res = [(self.uri, RDF.type, FOOD.Recipe),
-                (self.uri, RDF.type, SCHEMA.Recipe),
-                (self.uri, SCHEMA.description, Literal(self.description, lang='en')),
-                (self.uri, SCHEMA.prepTime, Literal(self.prepTime, datatype=SCHEMA.Duration)),
-                (self.uri, SCHEMA.cookTime, Literal(self.cookTime, datatype=SCHEMA.Duration)),
-                (self.uri, FOOD.serves, Literal(self.servings, datatype=XSD.String))]
+               (self.uri, RDF.type, SCHEMA.Recipe),
+               (self.uri, SCHEMA.description, Literal(self.description, lang='en')),
+               (self.uri, SCHEMA.prepTime, Literal(self.prepTime, datatype=SCHEMA.Duration)),
+               (self.uri, SCHEMA.cookTime, Literal(self.cookTime, datatype=SCHEMA.Duration)),
+               (self.uri, FOOD.serves, Literal(self.servings, datatype=XSD.String))]
 
         nutrients = {}
         for ingredient in self.ingredients:
-            for nutrient in ingredient.nutrients:
-              if not nutrients.get(nutrient.get('name')):
-                nutrients[nutrient.get('name')] = 0
-              nutrients[nutrient.get('name')] += float(nutrient.get('value'))
-
             res.append((self.uri, FOOD.ingredient, ingredient.uri))
-        print(nutrients)
-        # TODO: add other fields to the graph
+
+            for nutrient in ingredient.nutrients:
+                if nutrient.get('nutrient_id') is not 268:
+                    if not nutrients.get(nutrient.get('name')):
+                        nutrients[nutrient.get('name')] = {'count': 0, 'unit': nutrient.get('unit')}
+                    nutrients[nutrient.get('name')]['count'] += float(nutrient.get('value'))
+
+        # TODO: transFatContent and unsaturatedFatContent are unknow
+        for nutritionalInformation in nutrients:
+            if nutritionalInformation == 'Energy':
+                res.append((self.uri, SCHEMA.calories, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Energy)))
+            if nutritionalInformation == 'Carbohydrate, by difference':
+                res.append((self.uri, SCHEMA.carbohydrateContent, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Mass)))
+            if nutritionalInformation == 'Cholesterol':
+                res.append((self.uri, SCHEMA.cholesterolContent, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Mass)))
+            if nutritionalInformation == 'Total lipid (fat)':
+                res.append((self.uri, SCHEMA.fatContent, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Mass)))
+            if nutritionalInformation == 'Fiber, total dietary':
+                res.append((self.uri, SCHEMA.fiberContent, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Mass)))
+            if nutritionalInformation == 'Protein':
+                res.append((self.uri, SCHEMA.proteinContent, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Mass)))
+            if nutritionalInformation == 'Fatty acids, total saturated':
+                res.append((self.uri, SCHEMA.saturatedFatContent, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Mass)))
+            if nutritionalInformation == 'Sodium, Na':
+                res.append((self.uri, SCHEMA.sodiumContent, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Mass)))
+            if nutritionalInformation == 'Sugars, total':
+                res.append((self.uri, SCHEMA.sugarContent, Literal('{} {}'.format(nutrients[nutritionalInformation]['count'], nutrients[nutritionalInformation]['unit']), datatype=SCHEMA.Mass)))
+
+        # TODO: add steps to the graph
         return res
